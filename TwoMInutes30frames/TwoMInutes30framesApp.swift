7d5abfb7ae291190f47aac41d578a26e94f7036b
Harshit Agarwal <harshitagarwal46446@icloud.com>
2024-02-13 15:26:40 +0530

    Initial Commit

---
+++ TwoMInutes30frames/TwoMInutes30framesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwoMInutes30framesApp.swift
+//  TwoMInutes30frames
+//
+//  Created by Harshit Agarwal on 13/02/24.
+//
+
+import SwiftUI
+
+@main
+struct TwoMInutes30framesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
